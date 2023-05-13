8934ab08aa2054660574cd60ec7617a80b5df0e4
MatthewAndresWestMec <mandre361@west-mec.org>
2023-05-13 13:14:11 -0700

    completed components, titles, navigations, & font
    (need media queries)

--- JQuery/fullPageScrolling.js
+++ JQuery/fullPageScrolling.js
@@ -2,5 +2,5 @@ new fullpage('#fullpage', {
                 autoScrolling: true,
                 navigation: true,
                 anchors: ['hero','about','resume','projects','contact'],
-                navigationTooltips: ['hero','about','resume','projects','contact']
+                navigationTooltips: ['Hero','About','Resume','Projects','Contact']
             })
