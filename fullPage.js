f3278f09f8aecfec31756414df695a0fffa94cc7
MatthewAndresWestMec <mandre361@west-mec.org>
2023-05-12 19:59:51 -0700

    full page scrolling test works!
    fullpage scrolling is a js library

---
+++ fullPage.js
@@ -0,0 +1,36 @@
+new fullpage('#fullpage', {
+    autoScrolling: true})
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="./styles/css/index.css">
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.19/fullpage.min.css"
+//         integrity="sha512-DbP1b6q56Ud5q0L+lvw9COyyH/+pN1lVmySvun4UsH8vmfdIHou+PAHfMnGXf6Fvd9rYLjSojeuPhQz2euU1cw=="
+//         crossorigin="anonymous" referrerpolicy="no-referrer" />
+//     <title>Portfolio</title>
+// </head>
+
+// <body>
+
+//     <div id="fullpage">
+//         <div class="section s1"><h1>sec</h1></div>
+//         <div class="section"><h1>sec</h1></div>
+//         <div class="section"><h1>sec</h1></div>
+//         <div class="section"><h1>sec</h1></div>
+//     </div>
+
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.19/fullpage.min.js"
+//         integrity="sha512-GS3CSFQ4aj+0fL2a0wV1ouvpdbTZ3eEUriXTpXt50dc0CkO1fgyWuLoPA5NWy1PAWmcaZNneM+OAThgCqPnX+g=="
+//         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+//         <script src="fullPage.js"> 
+//         new fullpage('#fullPage', {
+//     autoScrolling: true})</script>
+// </body>
+
+// </html>
